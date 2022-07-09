c421bbb58ae22d292e96de8d98ee5e9b06160c0a
Nathaniel Cornejo <Nathaniel.cor@gmail.com>
2022-07-08 23:37:45 -0700

    God is good2

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
-const Article = require('./models/article2')
+const Article = require('./models/article')
 const articleRouter = require('./routes/articles')
 const methodOverride = require('method-override')
 const app = express()
